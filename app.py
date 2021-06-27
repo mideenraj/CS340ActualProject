99b9dd3136bcacabb87207611a1ae7aeade9bf09
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-06-27 13:59:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def bsg_people():
     # Cursor acts as the person typing the specified command into MySQL
     cursor = db.execute_query(db_connection=db_connection, query=query)
     results = cursor.fetchall()     # I think this is a list of
-    print(results)
+    print(type(results))
     return render_template("bsg.j2", bsg_people=results)
 
 # Listener

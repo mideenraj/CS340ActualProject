226926307992e6b0ecf4110a3e85c5daf1ec8a1d
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-06-27 14:02:01 -0400

    adff

--- app.py
+++ app.py
@@ -2,31 +2,36 @@ from flask import Flask, render_template, json
 import os
 import database.db_connector as db
 
-# Configuration
-
+# -------------------- Initialization --------------------
 app = Flask(__name__)
 db_connection = db.connect_to_database()
 
-# Routes 
 
+# ------------------- Routes --------------------
 @app.route('/')
 def root():
     return render_template("main.j2")
 
 @app.route('/bsg-people')
 def bsg_people():
+    # The actual query...
     query = "SELECT * FROM bsg_people;"
+
     # Cursor acts as the person typing the specified command into MySQL
     cursor = db.execute_query(db_connection=db_connection, query=query)
-    results = cursor.fetchall()     # I think this is a list of
-    print(type(results))
+
+    # This returns a tuple of rows from query
+    results = cursor.fetchall()
+
+    # The specified file is rendered with the queried data
     return render_template("bsg.j2", bsg_people=results)
 
-# Listener
 
+
+
+
+
+# -------------------- Listener --------------------
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 1027))
     app.run(port=port, debug=True)
-
-
-# more testing stuff

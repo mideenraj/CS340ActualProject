139598ad7c44beb3163ed157e691eda372519f60
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-07-23 16:33:56 -0400

    asdfg

--- app.py
+++ app.py
@@ -8,9 +8,32 @@ db_connection = db.connect_to_database()
 
 
 # ------------------- Routes --------------------
-@app.route('/')
+@app.route('/')             # Home page (Reports)
 def root():
-    return render_template("main.j2")
+    return render_template("index.j2")
+
+@app.route('/test')
+def Customers():
+
+    # Step 1: Write query
+    query = "SELECT * FROM Customers;"
+
+    # Step 2: Send query ('Cursor' acts as the person typing the specified command into MySQL)
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+
+    # Step 3: Access result (This returns a tuple of selected rows from query)
+    results = cursor.fetchall()
+
+    print(f"My Test:\n {results}")
+
+    # The specified file is rendered with the queried data
+    return render_template("bsg.j2", bsg_people=results)
+
+    # The specified file is rendered with the queried data
+
+
+
+
 
 @app.route('/bsg-people')
 def bsg_people():
@@ -25,12 +48,6 @@ def bsg_people():
 
     # The specified file is rendered with the queried data
     return render_template("bsg.j2", bsg_people=results)
-
-@app.route('/test')
-def test():
-
-    # The specified file is rendered with the queried data
-    return render_template("test.j2")
 
 
 

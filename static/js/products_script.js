b91743f1b8c43b84122def207e006c635eca1c41
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-07-29 14:47:52 -0400

    ad

--- static/js/products_script.js
+++ static/js/products_script.js
@@ -1,10 +1,8 @@
 
 
-console.log("YES");
 
 document.addEventListener('DOMContentLoaded', async () => {
     //'Update' button event listener(s)
-    console.log("YES");
     document.querySelectorAll(".update_button").forEach(item => {item.addEventListener('click', update_product)});
 
 });
@@ -19,6 +17,6 @@ document.addEventListener('DOMContentLoaded', async () => {
 // ----------------- Function(s) block -----------------
 // Function 1: Update product callback function
 async function update_product (){
-    console.log("THIS IS A TEST!")
-    console.log(this.id)
-}
+    //console.log("THIS IS A TEST!")
+    //console.log(this.id)
+};

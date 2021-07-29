1056dae6f3798435dcf12f72abc5f42aa22a7209
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-07-29 14:13:26 -0400

    asdf

--- static/js/products_script.js
+++ static/js/products_script.js
@@ -3,9 +3,7 @@
 
 document.addEventListener('DOMContentLoaded', async () => {
     //'Update' button event listener(s)
-    var update_buttons = document.querySelectorAll(".update_button").forEach(item => {item.addEventListener('click', update_buttons);
-        
-    });
+    document.querySelectorAll(".update_button").forEach(item => {item.addEventListener('click', update_buttons)});
 
 })
 

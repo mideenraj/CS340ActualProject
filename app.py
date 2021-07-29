79ab1f8c8e88ecbad9a2e719c8526ce5d71b61a9
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-07-29 14:58:14 -0400

    asdf

--- app.py
+++ app.py
@@ -101,32 +101,38 @@ def load_orders():
 
 
 # Route 4: 'Products' subpage
-@app.route('/products')
+@app.route('/products', methods=['POST', 'GET'])
 def load_products():
 
-    # Step 1: Write query
-    query = "SELECT * FROM Products;"
+    if request.method == 'GET':
+        print("GET")
+        # Step 1: Write query
+        query = "SELECT * FROM Products;"
 
-    # Step 2: Send query ('Cursor' acts as the person typing the specified command into MySQL)
-    cursor = db.execute_query(db_connection=db_connection, query=query)
+        # Step 2: Send query ('Cursor' acts as the person typing the specified command into MySQL)
+        cursor = db.execute_query(db_connection=db_connection, query=query)
 
-    # Step 3: Access result (This returns a tuple of selected rows from query)
-    results = cursor.fetchall()
-    payload = []
-    payload.append(results)
+        # Step 3: Access result (This returns a tuple of selected rows from query)
+        results = cursor.fetchall()
+        payload = []
+        payload.append(results)
 
-    # Step 4: Print query results if Debugging
-    debug = False
-    if debug:
-        print("\n")
-        print(f"Type:{type(results)}")
-        print(f"Length: {len(results)}")
-        print("Result:")
-        for row in results:
-            print(row)
+        # Step 4: Print query results if Debugging
+        debug = False
+        if debug:
+            print("\n")
+            print(f"Type:{type(results)}")
+            print(f"Length: {len(results)}")
+            print("Result:")
+            for row in results:
+                print(row)
 
-    # Step 5: The specified file is rendered with the queried data
-    return render_template("products_subpage.j2", product_data=payload)
+        # Step 5: The specified file is rendered with the queried data
+        return render_template("products_subpage.j2", product_data=payload)
+
+    elif request.method == 'POST':
+        print("POST")
+        pass
 
 
 # Route 5: 'Departments' subpage

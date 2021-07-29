705a5eb1d0dedac72c3f7abb139c1ace9cafe8d2
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-07-29 16:35:08 -0400

    sfg

--- static/js/products_script.js
+++ static/js/products_script.js
@@ -20,9 +20,80 @@ document.addEventListener('DOMContentLoaded', async () => {
 async function update_product (){
     productID = this.id
 
-    // Change color and text of 'update' button that was clicked
-    this.style.backgroundcolor = "yellow"
-    this.textontent = "Change"
+
+    // Determine row to edit
+    all_rows = document.querySelectorAll(".product_row")
+    for (var row of all_rows){
+        if (row.id == productID){
+            rowToEdit = row
+            break
+        }       
+    }
+
+
+    // ------ Step 1: create edit row ------
+    // --Access current value of respective rows in selected row
+    rowCells = rowToEdit.children
+    var old_name = rowCells.item(1).textContent
+    var old_dID = rowCells.item(2).textContent
+    var old_price = rowCells.item(3).textContent
+    var old_unit = rowCells.item(4).textContent
+
+    // --Create elements
+    var editRow = document.createElement('tr')              //New row
+
+    // --Modify and append elements
+        // --PlaceHolder (for productID column)
+    var placeholderCell = document.createElement('td')      //Data cell
+    editRow.appendChild(placeholderCell)
+        // --Name
+    var nameEditCell = document.createElement('td')         //Data cell
+    var nameInputBox = document.createElement('input')      //Text box
+    nameInputBox.setAttribute('type', 'text')
+    nameInputBox.setAttribute('name', 'newName')
+    nameInputBox.setAttribute('textContent', old_name)
+    nameEditCell.appendChild(nameInputBox)
+    editRow.appendChild(nameEditCell)
+        // --DepartMent ID
+    var departmentEditCell = document.createElement('td')         //Data cell
+    var departmentInputBox = document.createElement('input')      //Text box
+    departmentInputBox.setAttribute('type', 'text')
+    departmentInputBox.setAttribute('name', 'newDepartment')
+    departmentInputBox.setAttribute('textContent', old_dID)
+    departmentEditCell.appendChild(departmentInputBox)
+    editRow.appendChild(departmentEditCell)
+        // --Sale Price
+    var priceEditCell = document.createElement('td')         //Data cell
+    var priceInputBox = document.createElement('input')      //Text box
+    priceInputBox.setAttribute('type', 'text')
+    priceInputBox.setAttribute('name', 'newPrice')
+    priceInputBox.setAttribute('textContent', old_price)
+    priceEditCell.appendChild(priceInputBox)
+    editRow.appendChild(priceEditCell)
+        // --Unit Type
+    var unitEditCell = document.createElement('td')         //Data cell
+    var unitInputBox = document.createElement('input')      //Text box
+    unitInputBox.setAttribute('type', 'text')
+    unitInputBox.setAttribute('name', 'newUnit')
+    unitInputBox.setAttribute('textContent', old_unit)
+    unitEditCell.appendChild(unitInputBox)
+    editRow.appendChild(unitEditCell)
+
+    // --Append row to Table (right underneath the row that is being edited)
+    rowToEdit.insertAdjacentElement('afterend', editRow)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

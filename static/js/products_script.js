b2fa4028401349a1c54e874ad5015b5dfd95e387
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-07-29 14:08:03 -0400

    adf

--- static/js/products_script.js
+++ static/js/products_script.js
@@ -2,9 +2,22 @@
 
 
 document.addEventListener('DOMContentLoaded', async () => {
-    //Update button event listener(s)
-    var update_buttons = document.getElementsByClassName("update_button");
-    console.log(update_buttons)
+    //'Update' button event listener(s)
+    var update_buttons = document.getElementsByClassName("update_button").addEventListener('click', update_product);
 
 })
 
+
+
+
+
+
+
+
+// ----------------- Function(s) block -----------------
+
+// Function 1: Update product callback function
+async function update_product (){
+    console.log("THIS IS A TEST!")
+    console.log(this.id)
+}

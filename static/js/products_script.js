b4844766a72e21e5311130a85ce23ba660710e49
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-07-29 13:59:40 -0400

    adsf

---
+++ static/js/products_script.js
@@ -0,0 +1,10 @@
+
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    //Update button event listener(s)
+    var update_buttons = document.getElementsByClassName("update_button");
+    console.log(update_buttons)
+
+})
+

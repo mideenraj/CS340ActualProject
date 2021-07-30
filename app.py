724f792e467a5e714182eea5e50b1cba5dd5a79c
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-07-30 15:00:34 -0400

    adf

--- app.py
+++ app.py
@@ -134,13 +134,31 @@ def load_orders():
             price = float(result2[0]['salePrice'])             # Since salePrice is of Decimal Type, change it to str
             total += (price * int(prod[1]))
 
-
-
         # Step 3: Execute the order (aka insert into 'Orders')
         query3 = f"INSERT INTO Orders VALUES ('0', '{response_obj['customer']}', '{seasonID}', '{total}');"
         cursor3 = db.execute_query(db_connection=db_connection, query=query3)
         result3 = cursor3.fetchall()
-        print("TEST_4:", result3)
+
+        query3 = f"SELECT LAST_INSERT_ID();"
+        cursor3 = db.execute_query(db_connection=db_connection, query=query3)
+        result3 = cursor3.fetchall()
+        print("TEST_5:", result3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         # Step 2:
         # Step 3:

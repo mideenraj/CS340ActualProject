4077ea63dcd8368529a108dd94e649c5bf1d71d3
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-07-30 17:20:11 -0400

    zsdfg

---
+++ static/js/reports_script.js
@@ -0,0 +1,29 @@
+
+
+// ----------------- Site URL construction -----------------
+// ---Step 1: Specify server and port number
+var flip_server = 1     // Choose the server you're using (1, 2, or 3)
+var port_num = 1027     // Choose a port number of your choice
+
+// ---Step 2: initialize variables (Don't change any of these)
+var baseURL = `http://flip${flip_server}.engr.oregonstate.edu:${port_num}/`
+var customers_subpage = baseURL + "customers"
+var orders_subpage = baseURL + "orders"
+var products_subpage = baseURL + "products"
+var departments_subpage = baseURL + "departments"
+var seasons_subpage = baseURL + "seasons"
+
+
+
+// Master event listener (On page Load)
+document.addEventListener('DOMContentLoaded', async () => {
+
+    
+
+});
+
+
+
+
+// ----------------------------------------------- Function(s) block -----------------------------------------------
+// Function 1: xxx

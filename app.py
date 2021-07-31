71056bb9fc867f3b6c898d9fb62dcdb86fccc95e
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-07-30 21:45:23 -0400

    asdf

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def root():
 
             # --Convert productID to productName
             for eachPS in currentAnnualStats:
-                query3 = f"SELECT productName FROM Products WHERE productID={eachPS['ProductID']};"
+                query3 = f"SELECT productName FROM Products WHERE productID='{eachPS['ProductID']}';"
                 cursor3 = db.execute_query(db_connection=db_connection, query=query3)
                 productName = cursor3.fetchall()[0]["productName"]
                 print("THIS:", productName)

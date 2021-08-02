7358f8c2fce353e2fd0c13c2781617e12a9674f4
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-08-01 21:45:43 -0400

    adf

--- static/js/seasons_script.js
+++ static/js/seasons_script.js
@@ -21,6 +21,8 @@ document.addEventListener('DOMContentLoaded', async () => {
     // 'Add Season' button event listener
     document.querySelector("#submit_button").addEventListener('click', addSeason);
 
+    
+
 });
 
 
@@ -43,11 +45,66 @@ async function addSeason(){
         body: JSON.stringify(payload),
         headers: {'Content-Type' : 'application/json'}
     }
-    console.log("TEST")
     var response = await fetch(url, fetchdata)
     var data = await response.json()
 
+    // -------- Step 2: Append data to table (By creating new row with the returned response)
+    var input_row = document.getElementById("input_row")        // Get the row before which the new row will be inserted
+    var new_row = document.createElement("tr")
+    new_row.setAttribute('class', 'season_row')
+    new_row.setAttribute('id', data['seasonID'])
 
+        // --Create ID cell
+    var id_cell = document.createElement('td')
+    id_cell.textContent = data['seasonID']
+    new_row.appendChild(id_cell)
+
+        // --Create Name cell
+    var name_cell = document.createElement('td')
+    name_cell.textContent = data['seasonName']
+    new_row.appendChild(name_cell)
+
+        // --Create Start date cell
+    var start_cell = document.createElement('td')
+    start_cell.textContent = data['startDate']
+    new_row.appendChild(start_cell)
+
+        // --Create End Date cell
+    var end_cell = document.createElement('td')
+    end_cell.textContent = data['endDate']
+    new_row.appendChild(end_cell)
+
+    /*
+        // --Create Modify cell (Buttons)
+    var button_cell = document.createElement('td')
+        // --Update Button
+    var update_butt = document.createElement('button')
+    update_butt.setAttribute('class', 'update_button')
+    update_butt.setAttribute('id', data['productID'])
+    update_butt.textContent = "Update"
+    button_cell.append(update_butt)
+        // --Delete Button
+    var delete_butt = document.createElement('button')
+    delete_butt.setAttribute('class', 'delete_button')
+    delete_butt.setAttribute('id', data['productID'])
+    delete_butt.textContent = "Delete"
+    button_cell.append(delete_butt)
+
+    new_row.append(button_cell)
+    */
+
+    input_row.insertAdjacentElement('beforebegin', new_row)  // --Append row to table
+
+    /*
+    // -------- Step 3: Add event listeners for the new buttons
+    update_butt.addEventListener('click', update_product)
+    delete_butt.addEventListener('click', delete_product)
+    */
+
+    // -------- Step 4: Clear input boxes
+    document.getElementById("sName").value = ""
+    document.getElementById("sDate").value = ""
+    document.getElementById("eDate").value = ""
 
     
 }

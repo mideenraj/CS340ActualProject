34e46c8f63c56b37bc071f1febecc333beb7e5d7
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-08-02 13:46:45 -0400

    adf

--- static/js/customers_script.js
+++ static/js/customers_script.js
@@ -3,7 +3,7 @@
 // ----------------- Site URL construction -----------------
 // ---Step 1: Specify server and port number
 var flip_server = 1     // Choose the server you're using (1, 2, or 3)
-var port_num = 1000     // Choose a port number of your choice
+var port_num = 10027     // Choose a port number of your choice
 
 // ---Step 2: initialize variables (Don't change any of these)
 var baseURL = `http://flip${flip_server}.engr.oregonstate.edu:${port_num}/`

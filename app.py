c285f4b836d0aa853b4db40a094c8f5ba8528773
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-08-02 12:19:37 -0400

    asdf

--- app.py
+++ app.py
@@ -171,8 +171,24 @@ def root():
             query2 = f"SELECT totalCost FROM Orders WHERE orderID='{response_obj['orderID']}';"
             cursor2 = db.execute_query(db_connection=db_connection, query=query2)
             totalPrice = float(cursor2.fetchall()[0]['totalCost'])
-            print("TRIG_0:", totalPrice)
+            #print("TRIG_0:", totalPrice)
 
+            # -------------Issue start from here downwards....
+
+            # -Second, after subtracting the amount from the order total, if value is 0, delete order entirely....
+            if totalPrice - float(response_obj['productTotal']) == 0:
+
+                print("OrderID:", response_obj['orderID'])
+                query3 = f"DELETE FROM Orders WHERE orderID='{response_obj['orderID']}';"
+                db.execute_query(db_connection=db_connection, query=query3)
+                print("TRIG_1: ORDER DELETED")
+
+            # -Third, ....Otherwise, simply update the Orders entry with the subtracted price
+            else:
+                updatedPrice = totalPrice - float(response_obj['productTotal'])
+                query4 = f"UPDATE Orders SET totalCost='{updatedPrice}' WHERE orderID='{response_obj['orderID']}';"
+                db.execute_query(db_connection=db_connection, query=query4)
+                print("TRIG_2: ORDER UPDATED")
 
             # ---Step 3: return
             return {"status":"complete"}

156dc376dfb81a3a98589d3b511518eff684125a
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-08-01 21:14:56 -0400

    adf

--- app.py
+++ app.py
@@ -562,7 +562,7 @@ def load_seasons():
         if response_obj["action"] == 'insert':
             # Step 1: Send query
             query = f"INSERT INTO Seasons (name, startDate, endDate) VALUES " \
-                    f"('{response_obj['sName']}', '{response_obj['sDate']}', '{response_obj['eDate']}');"
+                    f"('{response_obj['name']}', '{response_obj['start']}', '{response_obj['end']}');"
             cursor = db.execute_query(db_connection=db_connection, query=query)
             results = cursor.fetchall()
 

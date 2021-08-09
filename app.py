51601b38a74ad412ce430ff2bc21dbc74da08549
mideenraj <71846486+mideenraj@users.noreply.github.com>
2021-08-09 12:54:19 -0400

    zfdv

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def root():
             # Next, Get the seasonName from seasonID
             query = f"SELECT seasonName FROM Seasons WHERE seasonID='{eachEntry['seasonID']}';"
             cursor = db.execute_query(db_connection=db_connect_function(), query=query)
-            sName = cursor.fetchall()['seasonName']
+            sName = cursor.fetchall()[0]['seasonName']
             eachEntry['seasonName'] = sName
 
         payload.append(results)     # Append to payload
